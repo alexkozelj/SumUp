bd9281444976408d4028a0b2890b2c9065b8292d
alexkozelj <aleksandarkozelj@gmail.com>
2020-01-06 19:35:48 +0100

    Adding logData method in ItemCtrl and initializing App Ctrl

--- dist/css/app.js
+++ dist/css/app.js
@@ -13,17 +13,45 @@ const ItemCtrl = (function() {
   // Data Structure / State
   const data = {
     items: [
-      {id: 0, rank: '7', suit: 'hearts', value: 0}
-      {id: 5, rank: '10', suit: 'diams', value: 2}
+      {id: 0, rank: '7', suit: 'hearts', value: 0},
+      {id: 5, rank: '10', suit: 'diams', value: 2},
       {id: 8, rank: 'J', suit: 'spades', value: 1}
     ],
     currentItem: null,
     totalValue: 0
   }
+
+  // Public methods
+  return {
+    logData: function(){
+      return data;
+    }
+  }
+
 })();
 
 // UI Controller
-const UICtrl = (function() {})();
+const UICtrl = (function() {
+
+  // Public methods
+  return {
+
+  }
+
+})();
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
+const App = (function(ItemCtrl, UICtrl) {
+  
+  // Public methods
+  return {
+    init: function() {
+      console.log('Initialazing App');
+    }
+  }
+  
+})(ItemCtrl, UICtrl);
+
+
+// Initialize App
+App.init();
