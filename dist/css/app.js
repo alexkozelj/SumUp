6146c054db721cb9ad80513cf2040cb173c0eb51
alexkozelj <aleksandarkozelj@gmail.com>
2020-01-06 18:59:33 +0100

    Created card selection constructor and data structure

--- dist/css/app.js
+++ dist/css/app.js
@@ -1,21 +1,29 @@
 // Storage Controller
 
-
 // Item Controller
-const ItemCtrl = (function(){
-  
-})();
+const ItemCtrl = (function() {
+  // Card Selection Constructor
+  const SelectedCard = function(id, rank, suit, value) {
+    this.id = id;
+    this.rank = rank;
+    this.suit = suit;
+    this.value = value;
+  };
 
+  // Data Structure / State
+  const data = {
+    items: [
+      {id: 0, rank: '7', suit: 'hearts', value: 0}
+      {id: 5, rank: '10', suit: 'diams', value: 2}
+      {id: 8, rank: 'J', suit: 'spades', value: 1}
+    ],
+    currentItem: null,
+    totalValue: 0
+  }
+})();
 
 // UI Controller
-const UICtrl = (function(){
-  
-})();
-
+const UICtrl = (function() {})();
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
-  
-})(ItemCtrl, UICtrl);
-
-
+const App = (function(ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
